4055ec2c2770459213303ab32534ce6fc5582b99
Sam <nixcorn8@gmail.com>
2019-08-30 12:12:27 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <HelloWorld />
   </div>
 </template>
 
@@ -21,8 +21,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,210 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  
+  <div class="ui container">
+    
+    <h1>K Basic Stats</h1>
+    <hr>
+ 
+
+    <div class="ui message blue">Enter the values in your discrete data one by one, then press <b>done</b> when you are finished. [must have atleast three values]</div>
+    
+    <br>
+
+    <div class="ui action input">
+      <input type="number" placeholder="Enter Data" v-model="val" @keydown.enter="addVal">
+      <button class="ui button" @click="addVal">Add</button>
+    </div>
+
+    <br>
+    <br>
+
+    <button class="ui button blue huge" @click="done">Done</button>
+    <button class="ui button red huge" @click="data = []">Reset</button>
+
+    <br>
+
+    <h3 v-if="data.length >= 1">Sorted Data</h3>
+
+
+    <div class="ui message blue" v-if="data.length >= 1">
+
+      <span v-for="(value, index) in data" :key="index">{{value}}&nbsp;&nbsp; </span> 
+
+    </div>
+
+    <br>
+
+    
+
+  
+
+    <br>
+
+
+  <!-- MODAL -->
+  <div class="ui basic modal">
+    <div class="ui header">
+      
+      Data Analysis
+
+    </div>
+    <div class="content">
+
+      <table class="ui celled table">
+        <tbody class="ui blue">
+
+          <tr class="">
+            <td>Mean</td>
+            <td>{{mean}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Mode</td>
+            <td>{{mode}}</td>
+         
+          </tr>
+
+          <tr>
+            <td>Range</td>
+            <td>{{range}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Minimum</td>
+            <td>{{min}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Maximum</td>
+            <td>{{max}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Median (Q2)</td>
+            <td>{{q2}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Inter Quartile Range (IQR)</td>
+            <td>{{iqr}}</td>
+          </tr>
+
+          <tr class="primary">
+            <td>Data Skewness</td>
+            <td>{{skew}}
+             
+              <span v-if="Number(skew) > 0">&nbsp; (right)</span>
+              <span v-if="Number(skew) < 0">&nbsp; (left)</span>
+              <span v-if="Number(skew) == 0">&nbsp; (symmetrical)</span>
+
+
+            
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+
+    </div>
+    <div class="actions">
+      <div class="ui red basic cancel inverted button">
+        Close
+      </div>
+    </div>
   </div>
+
+
+
+  </div>
+
 </template>
 
 <script>
+import {min, mean, max, interquartileRange, quantile, median, sampleSkewness, mode, range} from 'simple-statistics'
+
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+
+  data() {
+
+    return {
+
+      val: "",
+      data: [],
+      mode: "",
+      mean: "",
+      skew: "",
+      q2: "",
+      iqr: "",
+      min: "",
+      max: "",
+      range: ""
+
+    }
+
+  },
+
+  methods: {
+
+    addVal() {
+
+      if (this.val != "") {
+        this.data.push(Number(this.val))
+        this.val = ""
+        this.sort()
+      }
+
+    },
+
+    done() {
+
+      if (this.data.length > 2) {
+        this.calc()
+
+        $(document).ready(() => {
+            $('.ui.basic.modal').modal('show')
+        })
+
+      }
+
+    },
+
+    calc() {
+
+        
+       if (this.data.length >= 1) {
+
+
+        this.mean = mean(this.data)
+        this.mode = mode(this.data)
+        this.min = min(this.data)
+        this.max = max(this.data)
+        this.q2 = median(this.data)
+        this.iqr = interquartileRange(this.data)
+        this.range = this.max - this.min
+        this.skew = sampleSkewness(this.data)
+
+
+       }
+
+
+    },
+
+    sort() {
+
+      this.data.sort(function(a, b){return a-b});
+
+    }
+
   }
+
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  * {
+    transition: all 0.3s;
+  }
 </style>
